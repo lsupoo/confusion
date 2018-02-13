e677c3113f6b468976b9de7b89a6d2e89ab0d201
Luis Supo <lsupoo@gmail.com>
2018-02-12 23:36:37 -0500

    Configuring Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http'
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from  '@angular/platform-browser/animations'
+import { MaterialModule } from '@angular/material'
+import { FlexLayoutModule } from '@angular/flex-layout'
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
