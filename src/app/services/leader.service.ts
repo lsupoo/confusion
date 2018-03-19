0a9566d9bd6d7ff946d6765997a695d6dfa6c00a
Luis Alberto Supo Orihuela <lsupoori@everis.com>
2018-03-19 11:13:35 -0500

    Promise Part 2

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -8,14 +8,26 @@ export class LeaderService {
   constructor() { }
 
   getLeaders(): Promise<Leader[]> {
-    return Promise.resolve(Leaders);
+    // return Promise.resolve(Leaders);
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second delay
+        setTimeout(() => resolve(Leaders), 2000);
+    });
   }
 
   getLeader(id: number): Promise<Leader> {
-    return Promise.resolve(Leaders.filter((leader) => (leader.id === id))[0]);
+    // return Promise.resolve(Leaders.filter((leader) => (leader.id === id))[0]);
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second delay
+        setTimeout(() => resolve(Leaders.filter((leader) => (leader.id === id))[0]), 2000);
+    });
   }
 
   getFeaturedLeader(): Promise<Leader> {
-    return Promise.resolve(Leaders.filter((leader) => leader.featured)[0]);
+    // return Promise.resolve(Leaders.filter((leader) => leader.featured)[0]);
+    return  new Promise(resolve => {
+      // Simulate server latency with 2 second delay
+        setTimeout(() => resolve(Leaders.filter((leader) => leader.featured)[0]), 2000);
+    });
   }
 }
